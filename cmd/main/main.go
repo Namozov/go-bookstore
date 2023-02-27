0b92f06b965cf7d0bdb64280479db17e9c1bddd3
Namozov <namozovazizbek777@gmail.com>
2023-02-27 08:45:47 +0500

    add-main

--- cmd/main/main.go
+++ cmd/main/main.go
@@ -1,5 +1,15 @@
 package main
 
+import (
+	"log"
+	"net/http"
+	"github.com/gorilla/mux"
+	"github.com/jinzhu/gorm/dialects/mysql"
+	"github.com/Namozov/go-bookstore/pkg/routes"
+)
 func main(){
-	
+	r := mux.NewRouter()
+	routes.RegisterBookStoreRouters(r)
+	http.Handle("/", r)
+	log.Fatal(http.ListenAndServe("localhost:9010", r))
 }
