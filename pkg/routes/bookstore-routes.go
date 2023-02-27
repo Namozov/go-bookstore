e3dcd827a0d177ddd02bc6c702bd3554ec86474c
Namozov <namozovazizbek777@gmail.com>
2023-02-27 07:56:37 +0500

    add-routes

--- pkg/routes/bookstore-routes.go
+++ pkg/routes/bookstore-routes.go
@@ -1,5 +1,15 @@
 package routes
 
 import(
+	
 	"github.com/gorilla/mux"
+	"github.com/Namozov/go-bookstore/pkg/controllers" //"./controllers" shunaqa qilsaman bo'ladi
 )
+
+var RegisterBookStoreRouters = func (router *mux.Router)  {
+	router.HandleFunc("/book/", controllers.CreateBook).Methods("POSt")
+	router.HandleFunc("/book/", controllers.GetBook).Methods("GET")
+	router.HandleFunc("/book/{bookId}", controllers.GetBookById).Methods("GET")
+	router.HandleFunc("/book/{bookId}", controllers.UpdateBook).Methods("PUT")
+	router.HandleFunc("/book/{bookId}", controllers.Delete.Book).Methods("DELETE")
+}
