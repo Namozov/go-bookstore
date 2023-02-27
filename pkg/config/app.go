7f84b0f5f97b402e47ed826088691fddfcc02548
Namozov <namozovazizbek777@gmail.com>
2023-02-27 09:23:22 +0500

    update-config

--- pkg/config/app.go
+++ pkg/config/app.go
@@ -18,3 +18,27 @@ func Connect(){
 func GetDB() *gorm.DB{
 	return db
 }
+
+func (b *Book) CreateBook() *Book{
+	db.NewRecord(b)
+	db.Create(&b)
+	return b
+}
+
+func GetAllBooks() []Book{
+	var Books []Book
+	db.Find(&Books)
+	return Books
+}
+
+func GetBookById(ID int64)(*Book, *gorm.DB){
+	var getBook Book
+	db := db.Where("ID=?", ID).Find(&getBook)
+	return &getBook, db
+}
+
+func DeleteBook(ID int64) Book{
+	var book Book
+	db.Where("ID=?", ID).Delete(book)
+	return book
+}
