061ea75c353bcbbf051b1d01ef8c09c0873f3899
Namozov <namozovazizbek777@gmail.com>
2023-02-27 11:52:00 +0500

    update-models

--- pkg/models/book.go
+++ pkg/models/book.go
@@ -7,15 +7,40 @@ import (
 
 var db *gorm.DB
 
-type Book struct{
+type Book struct {
 	gorm.Model
-	Name string `gorm:""json:"name"`
-	Author string `json:"author"`
+	Name        string `gorm:""json:"name"`
+	Author      string `json:"author"`
 	Publication string `json:"publication"`
 }
 
-func init (){
+
+func init() { // db ni bog'lab beradi
 	config.Connect()
 	db = config.GetDB()
 	db.AutoMigrate(&Book{})
+}
+
+func (b *Book) CreateBook() *Book{
+	db.NewRecord(b)
+	db.Create(&b)
+	return b
+}
+
+func GetAllBooks() []Book{
+	var Books []Book
+	db.Find(&Books)
+	return Books
+}
+
+func GetBookById(ID int64)(*Book, *gorm.DB){
+	var getBook Book
+	db := db.Where("ID=?", ID).Find(&getBook)
+	return &getBook, db
+}
+
+func DeleteBook(ID int64) Book{
+	var book Book
+	db.Where("ID=?", ID).Delete(book)
+	return book
 }
