ce83a0cbdb27ef5f2c7418f1e1d42706b1b0774b
Namozov <salom_007@proton.me>
2023-03-06 09:41:31 +0500

    fix-problem

--- pkg/utils/utils.go
+++ pkg/utils/utils.go
@@ -2,14 +2,17 @@ package utils
 
 import (
 	"encoding/json"
+	"fmt"
 	"io/ioutil"
 	"net/http"
 )
 
 func ParseBody(r *http.Request, x interface{}){
-	if body, err := ioutil.ReadAll(r.Body); err != nil{
-		if err := json.Unmarshal([]byte(body), x); err != nil{ // unmarshal requestni x ga yozadi
-			return
-		}
+	body, err := ioutil.ReadAll(r.Body)
+	if  err != nil{
+		fmt.Println(err)
+	}
+	if err := json.Unmarshal([]byte(body), x); err != nil{ // unmarshal json ni go tiliga o'giradi
+		return
 	}
 }
